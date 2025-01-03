006b756015e156af260cd59e50b339ef634e1654
Imaamjij <58385239+imaamjij@users.noreply.github.com>
2025-01-03 11:35:00 +0530

    update

--- src/Hashing_Concept/Arr_check.java
+++ src/Hashing_Concept/Arr_check.java
@@ -1,11 +1,19 @@
 package Hashing_Concept;
 
+import javax.sound.midi.Soundbank;
+import java.util.Arrays;
+
 public class Arr_check {
     public static void main(String[] args) {
         int[] arr = {1,2,1,2,3,1,5,1};
         int num=1;
 
         System.out.println(arrCount(num,arr));
+
+        //check sort()
+        Arrays.sort(arr);
+        System.out.println("----");
+        System.out.println(Arrays.toString(arr));
     }
     static int arrCount(int num, int[] arr){
         int count=0;
---
+++ src/Sorting/SortFunc.java
@@ -0,0 +1,13 @@
+package Sorting;
+import java.util.Arrays;
+
+public class SortFunc {
+    public static void main(String[] args) {
+        int[] a={5,4,3,2,1};
+        Arrays.sort(a);
+
+        for(int i:a) {
+            System.out.print(i + " ");
+        }
+    }
+}
